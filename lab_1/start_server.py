5f96966cfe45008f6464a6225fa77d68aa118b99
Daria <dasha.rassadina1@gmail.com>
2019-10-10 15:03:07 +0300

    Update start_server.py

--- lab_1/start_server.py
+++ lab_1/start_server.py
@@ -10,7 +10,7 @@ def main():
     url = 'https://www.bbc.com/russian/topics/0f469e6a-d4a6-46f2-b727-2bd039cb6b53'
     html_page = parser.get_html_page(url)
     articles = parser.find_articles(html_page.text)
-    json_file_path = parser.publish_report('/Users/articles.json', articles)  # returns file's path
+    json_file_path = parser.publish_report('articles.json', articles)  # returns file's path
     file = open(json_file_path, encoding='UTF-8')
     json_string = file.read()
     file.close()
