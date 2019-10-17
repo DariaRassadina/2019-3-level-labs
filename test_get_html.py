bb2803adc44a9031928d607baee65f37eda66196
katabarsukova <51086881+katabarsukova@users.noreply.github.com>
2019-10-17 13:53:14 +0300

    Update test_get_html.py

--- test_get_html.py
+++ test_get_html.py
@@ -6,7 +6,9 @@ class MyTestCase(unittest.TestCase):
     def test_html_page(self):
         url = 'https://www.bbc.com/russian/topics/0f469e6a-d4a6-46f2-b727-2bd039cb6b53'
         a = get_html_page(url)
-        print(a)
+        a2 = get_html_page('https://www.bbc.com/russian/topics/0f469e6a-d4a6-46f2-b727-2bd039cb6b5njh')
+        print(a2)
+        self.assertEqual(a2.status_code, 404)
         self.assertEqual(a.status_code, 200)
 
 
